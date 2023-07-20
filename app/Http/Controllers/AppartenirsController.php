1d0ab22a7dafc9bfff0276204a56f9cedac210ef
CherParent <dieumenshamamba@gmail.com>
2023-07-20 17:24:14 +0200

    creation des migrations model et controllers

---
+++ app/Http/Controllers/AppartenirsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Appartenirs;
+use Illuminate\Http\Request;
+
+class AppartenirsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Appartenirs $appartenirs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Appartenirs $appartenirs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Appartenirs $appartenirs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Appartenirs $appartenirs)
+    {
+        //
+    }
+}
