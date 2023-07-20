fcb5ac5a4f3a139c59e356d0c8122bf587649acf
CherParent <dieumenshamamba@gmail.com>
2023-07-20 16:56:41 +0200

    initialisation

---
+++ resources/views/layouts/global-layouts.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>WICOM-SA</title>
+    <link rel="stylesheet" href="{!! url('template/vendors/typicons.font/font/typicons.css') !!}">
+    <link rel="stylesheet" href="{!! url('template/css/vertical-layout-light/style.css') !!}">
+    <link rel="shortcut icon" href="{!! url('template/images/favicon.png') !!}">
+    @livewireStyles
+  </head>
+  <body>
+    <div class="container-scroller">
+        @include('layouts.partials._navbar')
+        @include('layouts.partials._sidebar')
+        @yield('contenu')
+        @include('layouts.partials._footer')
+    </div>
+    @livewireScripts
+    <script src="{!! url('template/vendors/js/vendor.bundle.base.js')!!}"></script>
+    <script src="{!! url('template/js/off-canvas.js') !!}"></script>
+    <script src="{!! url('template/js/hoverable-collapse.js') !!}"></script>
+    <script src="{!! url('template/js/template.js') !!}"></script>
+    <script src="{!! url('template/js/settings.js') !!}"></script>
+    <script src="{!! url('template/js/todolist.js') !!}"></script>
+    <script src="{!! url('template/js/dashboard.js') !!}"></script>
+  </body>
+  @show
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.global-layouts')
+
+@section('contenu')
+ Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure temporibus doloribus quasi tenetur, inventore corrupti quod itaque quas nesciunt nulla eveniet sequi deleniti, id eius architecto non minus ab quos?
+@endsection
